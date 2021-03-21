920812311f324141c49647bc4d1b406a99489099
Levi Hilton <levihilton@levis-mbp.lan>
2021-03-20 18:41:43 -0600

    added the encoder button and changed the matrix a touch

--- firmware/keyboards/owl/config/left/keyboard_config.h
+++ firmware/keyboards/owl/config/left/keyboard_config.h
@@ -38,9 +38,9 @@ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR P
       k00, k01, k02, k03, k04, k05, k06,  \
       k10, k11, k12, k13, k14, k15, k16,  \
       k20, k21, k22, k23, k24, k25, k26,  \
-      k30, k31, k32, k33, k34, k35, k36,  \
-      k40, k41, k42, k43, k44, k45, k46, k66,  \
-                k52, k53, k54, k55, k56, k65,  \
+      k30, k31, k32, k33, k34, k35, k36, k46, \
+      k40, k41, k42, k43, k44, k45, k56, k66,  \
+                k51, k52, k53, k54, k55, k65,  \
                           k61, k62, k63, k64   \
 ) \
 { \
@@ -49,7 +49,7 @@ LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR P
     { k20, k21, k22, k23, k24, k25, k26 },  \
     { k30, k31, k32, k33, k34, k35, k36 },  \
     { k40, k41, k42, k43, k44, k45, k46 },  \
-    { KC_NO, KC_NO, k52, k53, k54, k55, k56 },  \
+    { KC_NO, k51, k52, k53, k54, k55, k56 },  \
     { KC_NO, k61, k62, k63, k64, k65, k66 }   \
 }
 #else
